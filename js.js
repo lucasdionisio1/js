13edc6564da0e3f8c171f6da49b6c4b67bac9561
lucas dionisio <144950155+lucasdionisio1@users.noreply.github.com>
2023-11-22 16:47:52 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,78 @@
+//let nomePersonagem = 'neymar'
+//let sobrenome = 'junior'
+//let idade = '31anos' 
+//let emprego = 'futebolSaudita'
+
+//console.log(nomePersonagem + sobrenome + idade + emprego )
+
+//console.log('meu nome é,' nomePersonagem, 'e meu sobrenome é', sobrenome, 'e minha idade é', idade, 'estou empregado', emprego,)
+
+//console.log(`meu nome e ${meuPersonagem} e meu sobrenome e ${sobrenome}
+//    minha idade ${idade} estou empregado ${emprego}`)
+
+
+
+//const nomeUsuatio =  prompt('digite o seu nome')
+
+//let idade = Number(prompt)('ditigea a sua idade')
+
+
+//console.log( typeof idade)
+
+//idade.tostring(idade)
+//console.log( typeof idade)
+
+//console.log(typeof nomeUsuario)
+
+//_______________________________________________________________________________________________________________________________________________________________________________________________________________________________________________
+
+let nome = 'nome'
+let cidade = 'cidade'
+ 
+console.log(typeof nome)
+console.log(typeof cidade)
+console.log(`Olá, meu nome é ${nome} e moro em ${cidade}.`)
+//------------------------------------------------------------
+let nome = prompt("Digite seu nome:")
+console.log(typeof nome)
+ 
+let cidade = prompt("Digite sua cidade:")
+console.log(typeof cidade)
+ 
+console.log(`Olá, meu nome é ${nome} e moro em ${cidade}.`)
+ 
+ 
+// 2
+ 
+let pergunta = "voce gosta de paçoca? "
+let pergunta2 = "Você joga videogame? "
+let pergunta3 = "Você ja jogou futebol? "
+ 
+//let resposta = prompt(pergunta + "Sim ou não: ");
+//let resposta2 = prompt(pergunta2 + "Sim ou não: ");
+//let resposta3 = prompt(pergunta3 + "Sim ou não: ");
+ 
+console.log(pergunta + " " + resposta);
+console.log(pergunta2 + " " + resposta2);
+console.log(pergunta3 + " " + resposta3);
+ 
+ 
+let x = 5
+let y = 10
+let z
+z = x
+x = y 
+y = z
+
+console.log('troquei x = 10', x)
+console.log('troquei y = 5', y)
+//--------------------------------------------------------------------------------------------------------------------
+
+let primeiroNumero = Number(prompt(' insira seu primeiro numero'))
+let segundoNumero = Number(prompt('insira seu segundo numero'))
+
+let somar = primeiroNumero+segundoNumero
+console.log(somar)
+
+let multiplicar = primeiroNumero*segundoNumero
+console.log(multiplicar)
